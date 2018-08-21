a74fc258f012e84a5b31c98181748e2367a82a95
jjy1993612 <1004972871@qq.com>
2018-08-21 10:15:23 +0800

    聊天界面one

---
+++ chatlibrary/src/main/java/com/tima/code/Listener/IListener.java
@@ -0,0 +1,9 @@
+package com.tima.code.Listener;
+
+
+/**
+ * 发送广播接口,用于通知
+ */
+public interface IListener {
+    void notifyEvent(String eventStr, Object eventObj);
+}
---
+++ chatlibrary/src/main/java/com/tima/code/ui/ChatActivity.kt
@@ -0,0 +1,24 @@
+package com.tima.code.ui
+
+import android.os.Bundle
+import com.tima.code.R
+import com.tima.common.base.BaseActivity
+
+/**
+ * Created by Administrator on 2018/8/20/020.
+ */
+class ChatActivity : BaseActivity() {
+
+    override fun inits(savedInstanceState: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
+    }
+
+    override fun getLayoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return R.layout.activity_chat_layout
+
+    }
+
+}
