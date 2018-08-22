efcca7539b044929956cfc2eecd71cb82d80ce67
zhijun.li <zhijun.li@timanetworks.com>
2018-08-22 10:31:51 +0800

    Base加入EventBus

--- commonresource/src/main/java/com/tima/common/base/BaseActivity.kt
+++ commonresource/src/main/java/com/tima/common/base/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.zhy.autolayout.AutoLayoutActivity
+import org.greenrobot.eventbus.EventBus
 
 /**
  * @author : zhijun.li on 2018/6/27
@@ -23,9 +24,29 @@ abstract class BaseActivity : AutoLayoutActivity(){
         inits(savedInstanceState)
     }
 
+
+
     fun getRootView() : View?=rootView
+    //default is not use EventBus
+    fun userEventBus() :Boolean =false
 
     abstract fun getLayoutId() :Int
     abstract fun inits(savedInstanceState: Bundle?)
+
+    override fun onStart() {
+        super.onStart()
+        //if use  and is not register
+        if(userEventBus()&&!EventBus.getDefault().isRegistered(this)){
+            EventBus.getDefault().register(this)
+        }
+    }
+
+    override fun onDestroy() {
+        //if use and  is  register
+        if (userEventBus()&&EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this)
+        }
+        super.onDestroy()
+    }
 
 }
