49c2b341122fda9835a38f05b5dc7c579b0e475d
zhijun.li <zhijun.li@timanetworks.com>
2018-09-14 10:57:44 +0800

    地图冲突问题

--- commonresource/src/main/java/com/tima/common/alipay/AlipayUtils.kt
+++ commonresource/src/main/java/com/tima/common/alipay/AlipayUtils.kt
@@ -1,11 +1,12 @@
 package com.tima.common.alipay
 
+import android.text.TextUtils
 import com.alipay.sdk.app.PayTask
 import com.tima.common.utils.ActivityManage
 import io.reactivex.Observable
 import io.reactivex.ObservableOnSubscribe
-import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
+import org.jetbrains.anko.toast
 
 /**
  * @author : zhijun.li on 2018/9/14
@@ -13,26 +14,38 @@ import io.reactivex.schedulers.Schedulers
  *
  */
 object AlipayUtils {
-    fun pay(orderInfo: String, sign: String,listener :PayBackListener) {
+    fun pay(orderInfo: String, sign: String, listener: PayBackListener?) {
         val currentActivity = ActivityManage.instance.getCurrentActivity()
         currentActivity?.let { activity ->
             val payInfo = orderInfo + "&sign=\"" + sign + "\"&" + "sign_type=\"RSA\""
-            Observable.create(ObservableOnSubscribe<String> { emitter ->
+            Observable.create(ObservableOnSubscribe<Map<String, String>> { emitter ->
                 run {
                     val alipay = PayTask(activity)
-                    val result = alipay.pay(payInfo, true)
+                    val result = alipay.payV2(payInfo, true)
                     emitter.onNext(result)
                 }
             })
                     .observeOn(Schedulers.io())
                     .subscribeOn(Schedulers.newThread())
                     .subscribe {
-                        listener.payBack(it)
+                        //                        PayResult(it).getResult();// 同步返回需要验证的信息
+                        val resultStatus = PayResult(it).getResultStatus()
+                        if (TextUtils.equals(resultStatus, "9000")) {
+                            activity.toast("支付成功")
+                        } else {
+                            if (TextUtils.equals(resultStatus, "8000")) {
+                                activity.toast("支付结果确认中")
+                            } else {
+                                activity.toast("支付失败")
+                            }
+                        }
+                        listener?.payBack(resultStatus)
                     }
         }
 
     }
 }
-interface PayBackListener{
-    fun payBack(result: String)
+
+interface PayBackListener {
+    fun payBack(result: String?)
 }
---
+++ commonresource/src/main/java/com/tima/common/alipay/PayResult.kt
@@ -0,0 +1,44 @@
+package com.tima.common.alipay
+
+import android.text.TextUtils
+
+/**
+ * @author : zhijun.li on 2018/9/14
+ *   email : zhijun.li@timanetworks.com
+ *
+ */
+class PayResult(rawResult: Map<String, String>?) {
+    private var resultStatus: String? = null
+    private var result: String? = null
+    private var memo: String? = null
+
+    init {
+        rawResult?.apply {
+            keys.forEach {
+                if (TextUtils.equals(it, "resultStatus")) {
+                    resultStatus = get("resultStatus")
+                } else if (TextUtils.equals(it, "result")) {
+                    result = get("result")
+                } else if (TextUtils.equals(it, "memo")) {
+                    memo = get("memo")
+                }
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return "resultStatus={" + resultStatus + "};memo={" + memo + "};result={" + result + "}"
+    }
+
+    fun getResultStatus(): String? {
+        return resultStatus
+    }
+
+    fun getMemo(): String? {
+        return memo
+    }
+
+    fun getResult(): String? {
+        return result
+    }
+}
